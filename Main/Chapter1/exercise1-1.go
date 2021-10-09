db106021441e5e2cc2d0216e0a8e28e00072abde
suhal vemu <vemusuhal@gmail.com>
2021-10-09 13:34:58 +0530

    initial commit and exercise 1-1 solution

---
+++ Main/Chapter1/exercise1-1.go
@@ -0,0 +1,18 @@
+//modify the echo program to also print os.Args[0], the name of the command that invoked it.
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", ""
+	for _, arg := range os.Args[0:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+
+}
